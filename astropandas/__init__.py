04349fa83ea670b391b0d0fd3a9e5f64ff4bfdee
Jan Luca van den Busch <jl.vdb@astro.rub.de>
2023-06-26 16:56:23 +0200

    switched to setup.cfg and pyproject.toml

--- astropandas/__init__.py
+++ astropandas/__init__.py
@@ -1,3 +1,4 @@
-from .version import __version__
+__version__ = "1.2"
+
 from .io import read_fits, to_fits, read_auto, to_auto
 from .match import match
--- setup.py
+++ setup.py
@@ -1,28 +1,5 @@
-import os
-import setuptools
+from setuptools import setup
 
-source_dir = os.path.dirname(os.path.abspath(__file__))
 
-with open(os.path.join(source_dir, "README.md")) as f:
-    long_description = f.read()
-
-with open(os.path.join(source_dir, "requirements.txt")) as f:
-    install_requires = [pkg.strip() for pkg in f.readlines() if pkg.strip()]
-
-__version__ = None
-with open(os.path.join(source_dir, "astropandas", "version.py")) as f:
-    exec(f.read())  # reads __version__ value
-
-setuptools.setup(
-    name="astropandas",
-    version=__version__,
-    author="Jan Luca van den Busch",
-    description="Tools to expand on pandas functionality for astronomical operations.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/jlvdb/astropandas",
-    packages=["astropandas"],
-    install_requires=install_requires,
-    classifiers=[
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Programming Language :: Python :: 3"])
+if __name__ == "__main__":
+    setup()
